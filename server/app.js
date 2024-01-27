9c64cc106ca35979c955996e356383672d78c7ea
penszzip <pazhiloyflex@gmail.com>
2024-01-27 12:44:57 -0500

    mongo connections setup

--- server/app.js
+++ server/app.js
@@ -6,7 +6,13 @@ const mongoose = require("mongoose");
 const middleware = require("./utils/middleware");
 const pledgeRouter = require("./routes/pledge");
 
-mongoose.connect(process.env.MONGODB_URI);
+const db_url = process.env.MONGODB_URI
+mongoose.connect("mongodb+srv://pledgeuser:W0v4e2Q5IgyE8zwP@pledges-cluster.baj3pce.mongodb.net/?retryWrites=true&w=majority");
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
 
 app.use(cors());
 app.use(express.json());
