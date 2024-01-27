6a4e1daa4a39472c9b2e0635cf2bfb7435a8f6c8
Abdul <lhlrahman@gmail.com>
2024-01-27 16:08:39 -0500

    complete the Add Pledge Modal

--- client/src/Components/Modal.jsx
+++ client/src/Components/Modal.jsx
@@ -15,12 +15,14 @@ export default function Modal(){
         priority: "",
     });
 
-    const inputStyle = "bg-[#2A2A2A] text-[#838383] p-3 rounded-lg border rounded-xl border-black w-full ";
-
+    const inputStyle = "bg-[#2A2A2A] text-[#838383] p-4 rounded-lg border rounded-xl border-black w-full ";
   return (
     <div className="fixed inset-0 flex items-center justify-center z-50">
       <div className="bg-[#1e1E1E] rounded-3xl p-6 w-4/6">
-      <span className=" p-1 px-4 cursor-pointer border rounded-full bg-[#838383]"> X </span>
+      <div className="flex">
+        <span className="flex items-start p-1 px-4 cursor-pointer border rounded-full bg-[#838383] mr-auto"> X </span>
+        <span className="flex items-end p-1 px-4 cursor-pointer border rounded-full bg-[#838383] ml-auto"> Add</span>
+        </div>
         <div className=" pt-4 *:text-center">
           <div className='text-black'>
             <div className="bg-[#2A2A2A] text-[#838383] border rounded-xl flex flex-col mb-4 border-black">
@@ -95,51 +97,52 @@ export default function Modal(){
                   location: e.target.value
                 }))}/>
             </div>
-        <div className="grid grid-rows-3">
-            </div>
-            <div className="mb-4">
-              <textarea
-                className={inputStyle}
+            <div className="flex flex-row text-[#838383]">
+            <div className="mr-2">
+                <textarea
                 id="repeat"
                 name="repeat"
                 placeholder="repeat"
-                type="option"
-                onChange={e => setPledgeData((prev) => ({
-                  ...prev,
-                  description: e.target.value
+                type="text" // TODO: change to dropdown
+                className="bg-[#2A2A2A] bg-transparent p-2 border rounded-2xl border-[#3F3F3F] w-40 h-10"
+                onChange={e => setPledgeData(prev => ({
+                    ...prev,
+                    description: e.target.value
                 }))}
-              />
+                />
             </div>
-            <div className="mb-4">
-              <input
-                className={inputStyle}
+            <div className="mr-2">
+                <input
                 id="link"
                 name="link"
                 placeholder="link"
                 type="text"
-                onChange={e => setPledgeData((prev) => ({
-                  ...prev,
-                  link: e.target.value
-                }))}/>
+                className="bg-[#2A2A2A] bg-transparent p-2 border rounded-2xl border-[#3F3F3F] w-40 h-10"
+                onChange={e => setPledgeData(prev => ({
+                    ...prev,
+                    link: e.target.value
+                }))}
+                />
             </div>
-            <div className="mb-4">
+            <div className="mr-2">
                 <select
-                    className={inputStyle}
-                    id="priority"
-                    name="priority"
-                    onChange={e => setPledgeData(prev => ({
+                className="bg-[#2A2A2A] p-2 border rounded-2xl border-[#3F3F3F] w-40 h-10"
+                id="priority"
+                name="priority"
+                onChange={e => setPledgeData(prev => ({
                     ...prev,
                     priority: e.target.value
-                    }))}
+                }))}
                 >
-                    <option value="Low">Low</option>
-                    <option value="Medium">Medium</option>
-                    <option value="High">High</option>
+                <option value="Low">Low</option>
+                <option value="Medium">Medium</option>
+                <option value="High">High</option>
                 </select>
-                </div>
-        </div>
             </div>
-            
+            </div>
+            </div>
+        </div>
+
         </div>
       </div>
   );
